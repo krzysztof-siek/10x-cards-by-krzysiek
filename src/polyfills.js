06d6d0445f316640288a413f008ec60b63a1bc8d
krzysztof_siek <krzysztof.siek@sembot.com>
2025-06-13 13:47:01 +0200

    test9

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,11 +1,10 @@
 // Polyfill for MessageChannel in Cloudflare Workers environment
-if (typeof MessageChannel === 'undefined') {
-  globalThis.MessageChannel = class MessageChannel {
-    constructor() {
-      this.port1 = {};
-      this.port2 = {};
-    }
+if (typeof MessageChannel === "undefined") {
+  // Implementacja zastępcza dla MessageChannel
+  globalThis.MessageChannel = function MessageChannel() {
+    this.port1 = {};
+    this.port2 = {};
   };
 }
 
-export {}; 
+export {};
